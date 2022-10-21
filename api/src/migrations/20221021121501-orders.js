4c57cd268fefdce9a9c2c200674fc7f8eba3df61
Aleyna İlbeyi <aleynailbeyi@Aleyna-MacBook-Pro.local>
2022-10-21 17:22:20 +0300

    fixed migration

--- api/src/migrations/20221021121501-orders.js
+++ api/src/migrations/20221021121501-orders.js
@@ -3,7 +3,6 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    queryInterface.removeColumn('orders', 'status', { /* query options */ });
     /**
      * Add altering commands here.
      *
